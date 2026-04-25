3887443b3d489c0caad7a6d601d256eae286b2c2
justgasland <abdulgaffarsonola@gmail.com>
2026-04-25 01:53:47 +0100

    user endpoints done

--- routes/auth.py
+++ routes/auth.py
@@ -3,7 +3,8 @@ from models import User, RefreshToken
 from config import Config
 from utils.passwords import hash_password, verify_password
 from utils.validators import validate_email, validate_password, validate_username
-from utils.serializers import user_to_dict, token_to_dict
+from utils.serializers import user_to_dict, token_to_dict, meta
+from utils.tokens import generate_access_token, generate_refresh_token, decode_token
 from database import SessionLocal
 from datetime import datetime, timedelta
 import uuid
@@ -135,135 +136,136 @@ def create_user():
 
 @authBlueprint.route('/auth/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        data = request.get_json()
-        if not data:
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            "success": False,
+            "message": "Invalid JSON payload.",
+            "errors": {
+                "code": "invalid_json",
+                "details": [{"field": None, "message": "Invalid JSON payload."}]
+            },
+            "meta": meta()
+        }), 400
+
+    email = data.get("email")
+    password = data.get("password")
+
+    email = email.lower() if email else None
+
+    errors = []
+
+    if not email:
+        errors.append({"field": "email", "message": "Email is required."})
+
+    if not password:
+        errors.append({"field": "password", "message": "Password is required."})
+
+    if errors:
+        return jsonify({
+            "success": False,
+            "message": "Validation Failed",
+            "errors": {
+                "code": "validation_error",
+                "details": errors
+            },
+            "meta": meta()
+        }), 422
+
+    session = SessionLocal()
+
+    try:
+        user = session.query(User).filter_by(email=email).first()
+
+        if not user:
             return jsonify({
                 "success": False,
-                "message": "Invalid JSON payload.",
+                "message": "Invalid email or password.",
                 "errors": {
-                    "code": "invalid_json",
-                    "details": [{"field": None, "message": "Invalid JSON payload."}]
+                    "code": "invalid_credentials",
+                    "details": [{"field": "email", "message": "Invalid email or password."}]
                 },
-                "meta": {
-                    "timestamp": datetime.utcnow().isoformat() + 'Z',
-                    "request_id": str(uuid.uuid4())
-                },
-            }), 400
-        email = data.get('email')
-        password = data.get('password')
+                "meta": meta()
+            }), 401
 
-        email = email.lower() if email is not None else None
-
-        session= SessionLocal()
-        try:
-            user = session.query(User).filter_by(email=email).first()
-            if not user:
-                return jsonify({
-                    "success": False,
-                    "message": "Invalid email or password.",
-                    "errors": {
-                        "code": "invalid_credentials",
-                        "details": [{"field": "email", "message": "Invalid email or password."}]
-                    },
-                    "meta": {
-                        "timestamp": datetime.utcnow().isoformat() + 'Z',
-                        "request_id": str(uuid.uuid4())
-                    },
-                }), 401
-
-            if not verify_password(password, user.password_hash):
-                return jsonify({
-                    "success": False,
-                    "message": "Invalid email or password.",
-                    "errors": {
-                        "code": "invalid_credentials",
-                        "details": [{"field": "password", "message": "Invalid email or password."}]
-                    },
-                    "meta": {
-                        "timestamp": datetime.utcnow().isoformat() + 'Z',
-                        "request_id": str(uuid.uuid4())
-                    },
-                }), 401
-            
-
-            if not user.is_active:
-                return jsonify({
-                    "success": False,
-                    "message": "Account is inactive. Please contact support.",
-                    "errors": {
-                        "code": "account_inactive",
-                        "details": [{"field": "email", "message": "Account is inactive. Please contact support."}]
-                    },
-                    "meta": {
-                        "timestamp": datetime.utcnow().isoformat() + 'Z',
-                        "request_id": str(uuid.uuid4())
-                    },
-                }), 403
-            
-            if user.deleted_at is not None:
-                return jsonify({
-                    "success": False,
-                    "message": "Account has been deleted.",
-                    "errors": {
-                        "code": "account_deleted",
-                        "details": [{"field": "email", "message": f"Account was deleted at {user.deleted_at.isoformat()}."}]
-                    },
-                    "meta": {
-                        "timestamp": datetime.utcnow().isoformat() + 'Z',
-                        "request_id": str(uuid.uuid4())
-                    },
-                }), 410
-
-            access_token = generate_access_token(user.id)
-            refresh_token = generate_refresh_token(user.id)
-
-            new_refresh_token_row = RefreshToken(
-                user_id=user.id,
-                token=refresh_token,
-                expires_at=datetime.utcnow() + timedelta(seconds=Config.JWT_REFRESH_TOKEN_EXPIRES),
-                device_info=request.headers.get('User-Agent', 'Unknown')
-            )
-
-            user.last_login_at = datetime.utcnow()
-
-            session.add(new_refresh_token_row)
-            session.commit()
-            session.refresh(new_refresh_token_row)
-
+        if not verify_password(password, user.password_hash):
             return jsonify({
+                "success": False,
+                "message": "Invalid email or password.",
+                "errors": {
+                    "code": "invalid_credentials",
+                    "details": [{"field": "password", "message": "Invalid email or password."}]
+                },
+                "meta": meta()
+            }), 401
+
+        if not user.is_active:
+            return jsonify({
+                "success": False,
+                "message": "Account is inactive.",
+                "errors": {
+                    "code": "account_inactive",
+                    "details": [{"field": "email", "message": "Account is inactive."}]
+                },
+                "meta": meta()
+            }), 403
+
+        if user.deleted_at is not None:
+            return jsonify({
+                "success": False,
+                "message": "Account has been deleted.",
+                "errors": {
+                    "code": "account_deleted",
+                    "details": [{"field": "email", "message": "Account has been deleted."}]
+                },
+                "meta": meta()
+            }), 410
+
+        access_token = generate_access_token(user.id)
+        refresh_token = generate_refresh_token(user.id)
+
+        refresh_token_row = RefreshToken(
+            user_id=user.id,
+            token=refresh_token,
+            expires_at=datetime.utcnow() + timedelta(seconds=Config.JWT_REFRESH_TOKEN_EXPIRES),
+            device_info=request.headers.get("User-Agent", "Unknown")
+        )
+
+        user.last_login_at = datetime.utcnow()
+
+        session.add(refresh_token_row)
+        session.commit()
+
+        return jsonify({
             "success": True,
             "message": "Login successful.",
-            "data":{
+            "data": {
                 "access_token": access_token,
-                "refresh_token": refresh_token, 
-                "user": user_to_dict(user)},
-            "meta": {
-                "timestamp": datetime.utcnow().isoformat() + 'Z',
-                "request_id": str(uuid.uuid4())
+                "refresh_token": refresh_token,
+                "token_type": "Bearer",
+                "expires_in": Config.JWT_ACCESS_TOKEN_EXPIRES,
+                "user": user_to_dict(user)
             },
+            "meta": meta()
         }), 200
 
+    except Exception as e:
+        print("LOGIN ERROR:", e)
+        session.rollback()
 
-        except Exception as e:
-            print("LOGIN ERROR:", e)
-            session.rollback()
+        return jsonify({
+            "success": False,
+            "message": "An error occurred while logging in.",
+            "errors": {
+                "code": "server_error",
+                "details": [{"field": None, "message": "An error occurred while logging in."}]
+            },
+            "meta": meta()
+        }), 500
 
-            return jsonify({
-                    "success": False,
-                    "message": "An error occurred while logging in.",
-                    "errors": {
-                        "code": "database_error",
-                        "details": [{"field": None, "message": "An error occurred while logging in."}]
-                    },
-                    "meta": {
-                        "timestamp": datetime.utcnow().isoformat() + 'Z',
-                        "request_id": str(uuid.uuid4())
-                    },
-                }), 500
-        finally:
-            session.close()
-
+    finally:
+        session.close()
         
 
 @authBlueprint.route('/auth/refresh', methods=['POST'])
--- routes/users.py
+++ routes/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, session
 from flask import jsonify, g
 
 from middleware.auth import require_auth
@@ -7,6 +7,10 @@ from utils.serializers import meta, user_to_dict
 from database import SessionLocal
 from datetime import datetime
 import uuid
+from utils.validators import validate_username, validate_email, validate_password, validate_role, validate_avatar_url, validate_bio ,validate_name
+
+
+
 usersBlueprint = Blueprint('users', __name__)
 
 @usersBlueprint.route("/users/me", methods=["GET"])
@@ -18,3 +22,206 @@ def get_me():
         "data": user_to_dict(g.user),
         "meta": meta()
     }), 200
+
+@usersBlueprint.route("/users/me", methods=["PATCH"])
+@require_auth
+def update_me():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            "success": False,
+            "message": "Invalid JSON payload.",
+            "errors": {
+                "code": "invalid_json",
+                "details": [{"field": None, "message": "Invalid JSON payload."}]
+            },
+            "meta": meta()
+        }), 400
+    allowed_fields = {
+    "username",
+    "first_name",
+    "last_name",
+    "bio",
+    "avatar_url"
+    }
+    for field in data.keys():
+        if field not in allowed_fields:
+            return jsonify({
+                "success": False,
+                "message": "Invalid field in payload.",
+                "errors": {
+                    "code": "invalid_field",
+                    "details": [{"field": field, "message": f"Field '{field}' is not allowed."}]
+                },
+                "meta": meta()
+            }), 400
+    username = data.get("username")
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    bio = data.get("bio")
+    avatar_url = data.get("avatar_url")
+
+
+    errors=[]
+    if username is not None:
+        username_error = validate_username(username)
+        if username_error:
+            errors.extend(username_error)
+    if first_name is not None:
+        first_name_error = validate_name(first_name, "first_name")
+        if first_name_error:
+            errors.extend(first_name_error)
+    if last_name is not None:
+        last_name_error = validate_name(last_name, "last_name")
+        if last_name_error:
+            errors.extend(last_name_error)
+    if bio is not None:
+        bio_error = validate_bio(bio)
+        if bio_error:
+            errors.extend(bio_error)
+    if avatar_url is not None:
+        avatar_url_error = validate_avatar_url(avatar_url)
+        if avatar_url_error:
+            errors.extend(avatar_url_error)
+    if errors:
+        return jsonify({
+            "success": False,
+            "message": "Validation errors.",
+            "errors": {
+                "code": "validation_error",
+                "details": errors
+            },
+            "meta": meta()
+        }), 400
+    
+
+    session = SessionLocal()
+    try:
+
+        user = session.query(User).filter_by(id=g.user.id).first()
+        if not user:
+            return jsonify({
+                "success": False,
+                "message": "User not found.",
+                "errors": {
+                    "code": "user_not_found",
+                    "details": [
+                        {
+                            "field": None,
+                            "message": "User not found."
+                        }
+                    ]
+                },
+                "meta": meta()
+                }), 404
+                    
+            
+        if username is not None and username != user.username:
+            existing_user = session.query(User).filter_by(username=username).first()
+            if existing_user and existing_user.id != user.id:
+                return jsonify({
+                    "success": False,
+                    "message": "Username already taken.",
+                    "errors": {
+                        "code": "username_taken",
+                        "details": [
+                            {
+                                "field": "username",
+                                "message": "Username is already taken by another user."
+                            }
+                        ]
+                    },
+                    "meta": meta()
+                }), 400
+            user.username = username
+
+
+        if first_name is not None:
+            user.first_name = first_name
+        if last_name is not None:
+            user.last_name = last_name
+        if bio is not None:
+            user.bio = bio
+        if avatar_url is not None:
+            user.avatar_url = avatar_url
+        session.commit()
+        return jsonify({
+            "success": True,
+            "message": "Profile updated successfully.",
+            "data": user_to_dict(user),
+            "meta": meta()
+        }), 200
+    
+
+    except Exception as e:
+        # print("PROFILE UPDATE ERROR:", e)
+        session.rollback()
+        return jsonify({
+            "success": False,
+            "message": "An error occurred while updating the profile.",
+            "errors": {
+                "code": "profile_update_error",
+                "details": [
+                    {
+                        "field": None,
+                        "message": "An unexpected error occurred while updating the profile. Please try again later."
+                    }
+                ]
+            },
+            "meta": meta()
+        }), 500
+    
+    finally:        
+        session.close()
+
+
+@usersBlueprint.route("/users/me", methods=["DELETE"])
+@require_auth
+def delete_me():
+    session = SessionLocal()
+    try:
+        user = session.query(User).filter_by(id=g.user.id).first()
+        if not user:
+            return jsonify({
+                "success": False,
+                "message": "User not found.",
+                "errors": {
+                    "code": "user_not_found",
+                    "details": [
+                        {
+                            "field": None,
+                            "message": "User not found."
+                        }
+                    ]
+                },
+                "meta": meta()
+            }), 404
+        
+        user.deleted_at = datetime.utcnow()
+        # user.acces
+        session.commit()
+        return jsonify({
+            "success": True,
+            "message": "Account deleted successfully.",
+            "meta": meta()
+        }), 200
+
+    except Exception as e:
+        session.rollback()
+        return jsonify({
+            "success": False,
+            "message": "An error occurred while deleting the account.",
+            "errors": {
+                "code": "account_deletion_error",
+                "details": [
+                    {
+                        "field": None,
+                        "message": "An unexpected error occurred while deleting the account. Please try again later."
+                    }
+                ]
+            },
+            "meta": meta()
+        }), 500
+    finally:
+        session.close()
